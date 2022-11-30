f7d75aeaad028fa1cd004ee92436e92b6b16f51d
mtbstn24 <thushibstn24@gmail.com>
2022-11-30 10:43:48 +0530

    add endpoint name

--- src/pages/applications.jsx
+++ src/pages/applications.jsx
@@ -30,7 +30,7 @@ export default function Applications() {
 
     try{
       axios.post(
-        'https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-prod.e1-us-east-azure.choreoapis.dev/ddrq/gramaconnect/1.0.0/',
+        'https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-prod.e1-us-east-azure.choreoapis.dev/ddrq/gramaconnect/1.0.0/updateStatus',
         form,
         {
           params: {
