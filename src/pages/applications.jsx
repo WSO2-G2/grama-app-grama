14cac592cb4940567cf60c719fb3bb1ba5f832f8
mtbstn24 <thushibstn24@gmail.com>
2022-11-30 11:05:35 +0530

    change body format

--- src/pages/applications.jsx
+++ src/pages/applications.jsx
@@ -31,7 +31,9 @@ export default function Applications() {
     try{
       axios.post(
         'https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-prod.e1-us-east-azure.choreoapis.dev/ddrq/gramaconnect/1.0.0/updateStatus',
-        form,
+        {
+          'status' : `${status}`
+        },
         {
           params: {
               'nic': `${data.nic}`,
@@ -40,9 +42,6 @@ export default function Applications() {
           headers: {
             'Authorization': `Bearer ${accessToken}`,
           },
-          body: {
-            'status' : `${status}`
-          }
         }
       ).then((res)=>{
         console.log(res);
